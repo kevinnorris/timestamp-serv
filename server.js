6513b15ec4ecf4e94385daaefe80498181904174
Kevin <kevin.norris.90@gmail.com>
2017-01-09 20:52:06 +0000

    Build microservice and landing page

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", function(req, res){
-   res.send("Hello World!"); 
+    //console.log("Got request: ", req.method, "\nHeaders: ", JSON.stringify(req.headers));
+    res.sendFile(path.join(__dirname, "/index.html")); 
 });
 
-app.listen(8080, function(){
-    console.log("Server listening on port 8080");
+app.get("/:data", function(req, res){
+    var data = req.params.data;
+    var date = parseInt(data) ? new Date(parseInt(data)) : new Date(data);
+    res.set("Content-Type", "application/json");
+    if(date.getTime()){
+        res.send({"unix": date.getTime(), "natural": date.toDateString()});
+    }else{
+        res.send({"unix": null, "natural": null});
+    }
+});
+
+app.listen(process.argv[2], function(){
+    console.log("Server listening on port ", process.argv[2]);
 });
